4f91e2aa23a7fed13d81ce26530eff9362bff43a
Gufran Mohammad <mgufran@yahoo.com>
2020-01-12 06:55:07 +0000

    commi

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -4,6 +4,7 @@ sap.ui.define([
 ], function (Controller, History) {
 	"use strict";
 //jxjx
+//rrr
 	return Controller.extend("mg.STA_MD_01.controller.BaseController", {
 		/**
 		 * Convenience method for accessing the router in every controller of the application.
